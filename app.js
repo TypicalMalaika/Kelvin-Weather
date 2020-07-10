5d5e0cee702d86dd6244d73b9781ac32a70a7682
Malaika Karim <42036621+TypicalMalaika@users.noreply.github.com>
2020-07-10 16:19:38 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+//Constant variable of 293 Kelvin
+const kelvin = 0;
+//Converts Kelvin into Celsius
+var celsius = kelvin-273;
+//Converts tp Fahrenheit
+var fahrenheit = celsius*(9/5)+32;
+//Rounding Fahrenheit down
+var roundFahrenheiht = Math.floor(fahrenheit);
+console.log(`The temperature is ${roundFahrenheiht} degrees Fahrenheit.`);
+var newton = celsius*(33/100);
+var newtonRounded = Math.floor(newton)
+console.log(`The temperature is ${newtonRounded} degrees Newton`)
